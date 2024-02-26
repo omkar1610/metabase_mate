fed8f125e2ca69fb1b4729da32cd7fd1a35653cf
omkar-work <omkara@flock.com>
2024-02-26 20:48:42 +0530

    updated v4

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='metabase-mate',
-    version='1.1.0',
+    version='4.0.0',
     packages=['metabase_mate'],
     url='https://github.com/omkar1610/metabase_mate',
     author='Omkar Ashrit',
     author_email='omkar.ashrit@gmail.com',
     description='A Python module for working with Metabase',
-    long_description='Mate is a Python utility module that automates repetitive tasks in the Metabase UI, saving time and effort. It provides functionality to edit existing questions by adding new linked filters and updating older filters. Additionally, Mate can replicate dashboards with linked filters and apply partner-specific or static conditions to each question within the dashboard.'
+    long_description=long_description,
+    long_description_content_type="text/markdown"
 )
